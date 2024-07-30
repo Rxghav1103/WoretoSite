2e02ed235f17f062691755afa4b9686838466ceb
Rxghav1103 <raghavendra.bhargava2004@gmail.com>
2024-07-30 23:09:24 +0530

    uploading all the initial files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./features.html", "./privacy_policy.html","download_page_real.html"], 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
